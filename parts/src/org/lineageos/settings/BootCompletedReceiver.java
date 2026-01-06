65ad47b4e12bece6b6681b527dcd8192c7db4a80
adithya2306 <adithya2306@users.noreply.github.com>
2026-01-06 09:37:19 +0530

    sm8450-common: Parts: thermal-profiles rewrite in kotlin
    Signed-off-by: pabloescobar-reborn <pabloescobarreborn77@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -34,7 +34,6 @@ import android.view.Display.HdrCapabilities;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
-import org.lineageos.settings.thermal.ThermalTileService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -80,11 +79,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.onBootCompleted(context);
 
         // Start Thermal Management Services
-        ThermalUtils.startService(context);
+        ThermalUtils.Companion.getInstance(context).startService();
 
         // Start Refresh Rate Service
         RefreshUtils.startService(context);
-
     }
 
     private void overrideHdrTypes(Context context) {
